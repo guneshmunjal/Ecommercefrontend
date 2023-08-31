d17681c200b3621324bc60f51ecac13cf0d3fa4f
gunesh <guneshmunjal09@gmail.com>
2023-08-31 17:26:28 +0530

    Your commit message here

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -8,7 +8,7 @@ export const Shop = () => {
   return (
     <div className='shop'>
         <div className='shopTitle'>
-            <h1>Pedrotech Shop</h1>
+            <h1>all in store</h1>
         </div>
         <div className='products'>
           {PRODUCTS.map((product)=>(
